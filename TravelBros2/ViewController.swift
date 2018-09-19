bb5d1fd2a5d5a56b6b57a3928ced8f1b2573fcbf
Damian Wozniak <damian.pwozniak@gmail.com>
2018-09-19 12:14:47 +0200

    Initial Commit

---
+++ TravelBros2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelBros2
+//
+//  Created by Peter on 19.09.2018.
+//  Copyright © 2018 Bros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
